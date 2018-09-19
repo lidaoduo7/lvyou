a74e601dd67e25e35bae94701ca4c754b9a23d7f
LiDao <1002850166@qq.com>
2018-09-19 16:12:26 +0800

    删除无用代码

--- lvyou/spiders/lvyou_spider.py
+++ lvyou/spiders/lvyou_spider.py
@@ -113,65 +113,6 @@ class LvyouSpiderSpider(scrapy.Spider):
         return results
 
 
-    # def parse_remark(self,response):
-    #     '''
-    #     解析评论
-    #     :param response:
-    #     :return:
-    #     '''
-    #     selector = Selector(response)
-    #     item = LvyouItem()
-    #     scene_name = self.get_scene_name(response.url)  # 景点的名称
-    #
-    #     numsum = selector.xpath('//*[@id="remark-container"]/div[1]/span/text()').extract()  # 条点评
-    #     # print(numsum[0])
-    #     group = re.findall(r"\d{1,2}", numsum[0])
-    #     remark_acccount = int(group[0])
-    #     # print(remark_acccount)
-    #
-    #     tags = selector.xpath('//div[@class="remark-item clearfix"]')
-    #     results = []
-    #     for tag in tags:
-    #         author = tag.xpath(
-    #             './/div[@class="ri-avatar-wrap"]//a[@class="ri-uname"]/text()').extract_first()  # 作者暂时爬不出来
-    #         time = tag.xpath(
-    #             './/div[@class="ri-main"]//div[@class="ri-header"]//div[@class="ri-time"]/text()').extract_first()
-    #         remark = tag.xpath('.//div[2]/div[2]/div[1]/text()').extract_first()  # 评论内容，内部有超链接，内容缺失
-    #
-    #         star_text= tag.xpath('.//div[@class="ri-main"]//div[@class="ri-header"]//div[@class="ri-rating"]//div/@class').extract_first()
-    #         group = re.findall(r"ri-star ri-star-(\d)", star_text)   #星级打分，基于AJAX，暂时爬不出来
-    #         star = str(group[0])
-    #         results.append((time,remark))
-    #         # https://lvyou.baidu.com/qianzhougucheng/remark/?rn=15&pn=15&style=hot#remark-container
-    #         # 翻页
-    #         if remark_acccount > 15:
-    #             for i in range(0, remark_acccount, 15):
-    #                 next_link = response.url + "?rn=15&pn=" + str(i) + "&style=hot#remark-container"
-    #                 yield scrapy.Request(next_link, callback=self.parse_remark)  # 超过15条评论的会写入多次
-    #         else:
-    #             # index_link = response.url + "?rn=15&pn=0&style=hot#remark-container"
-    #             # yield scrapy.Request(index_link, callback=self.parse_remark)
-    #             pass
-    #
-    #
-    #     remarks_list = []
-    #     for i in range(len(results)):
-    #         remark_time = results[i][0]
-    #         remark = results[i][1]
-    #         comments_dict = {'comment_time': remark_time, 'remark': remark}
-    #         remarks_list.append(comments_dict)
-    #     # item['remarks'] = remarks_list
-    #
-    #     source = "website"
-    #     item['source'] = source
-    #     second_source = "lvyou_baidu"
-    #     item['second_source'] = second_source
-    #     item['remark_acccount'] = remark_acccount
-    #     item['scene_name'] = scene_name
-    #     yield item
-
-
-
 
 
 
